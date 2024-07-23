d6acd13ecbecb5aa2bd3f70dd9edb2a5b20d5ccf
thienphamk1 <vanthien.dev@gmail.com>
2024-07-23 20:16:24 +0700

    change a little

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
 const { exec } = require('child_process');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 
 const inputVideo = 'input.mp4';
-
 const outputDirectory = 'output_hls';
 const outputFile = path.join(outputDirectory, 'output.m3u8');
 
-const ffmpegPath = path.join(__dirname, 'ffmpeg');
-
-
-// Tạo thư mục lưu hls 
 if (!fs.existsSync(outputDirectory)) {
-    try {
-        fs.mkdirSync(outputDirectory);
-    } catch (err) {
-        console.error(`Error creating output directory: ${err.message}`);
-        process.exit(1);
-    }
+    fs.mkdirSync(outputDirectory);
 }
 
+const ffmpegCommand = `ffmpeg -i ${inputVideo} -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls ${outputFile}`;
 
-const ffmpegCommand = `"${ffmpegPath}" -i "${inputVideo}" -codec: copy -start_number 0 -hls_time 10 -hls_list_size 0 -f hls "${outputFile}"`;
-
-
-// Thực thi lệnh FFmpeg
 exec(ffmpegCommand, (error, stdout, stderr) => {
     if (error) {
         console.error(`Error executing FFmpeg: ${error.message}`);
@@ -32,10 +19,9 @@ exec(ffmpegCommand, (error, stdout, stderr) => {
     }
 
     if (stderr) {
-        console.error(`FFmpeg stderr: ${stderr}`);
-        return;
+        console.log(`FFmpeg stderr: ${stderr}`);
     }
 
     console.log(`FFmpeg stdout: ${stdout}`);
-    console.log('Thành công!');
+    console.log('Chuyển đổi video thành công!');
 });
